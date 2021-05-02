3bd684fecff5a269c65a1ac829c913c41450eab1
Joel-Marc <joel.marceline3@gmail.com>
2021-05-02 15:00:21 +0530

    first end

--- 1.py
+++ 1.py
@@ -2,12 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from subprocess import check_output
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+finlist = []
 
 
 def popt(inp):
+    global finlist
+    intdict = {}
+    intdict['title'] = inp
     URL = "https://www.google.com/search?q="+inp
-    print(URL)
-#URL = "https://www.google.com/search?q=tenet+2020"
+    # print(URL)
+# URL = "https://www.google.com/search?q=tenet+2020"
     r = requests.get(URL)
 
     soup = BeautifulSoup(r.content, 'html5lib')
@@ -25,21 +33,29 @@ def popt(inp):
         l.append(links.get('href'))
 
     lf = []
+    temp = []
     for i in l:
         i = i[7:]
+        temp = i.split('&')
+        i = '<a href=\"' + temp[0] + '\">Click Here</a>'
         lf.append(i)
     # print(lf)
 
     te = re.split('\n|�', soup1.get_text())
-#te = [x for x in te if x != '�' and x != '']
+# te = [x for x in te if x != '�' and x != '']
     # print(te)
     for i in range(0, 5, 2):
         try:
-            print("")
-            print(te[i], te[i + 1], lf[int(i/2)])
-            print("")
+            # print("")
+            # print(te[i], te[i + 1], lf[int(i/2)])
+            intdict[te[i+1]] = te[i]
+            intdict[str('link'+str(int(i/2)))] = lf[int(i/2)]
+            # print(intdict[te[i+1]], te[i+1])
+            # print("")
         except LookupError:
             print("")
+    # print(intdict)
+    finlist.append(intdict)
 
 
 cmd = ['ls', '/mnt/c/Users/joelm/Downloads/ENTERTAINMENT/MOVIES/test']
@@ -47,6 +63,21 @@ x = check_output(r'dir "C:\Users\joelm\Downloads\ENTERTAINMENT\MOVIES\test" /B '
 y = x.split('\n')
 y = [x for x in y if x != '']
 # print(x)
-print(y)
+# print(y)
 for i in y:
     popt(i)
+
+# print(finlist)
+
+for k in finlist:
+    for za, zu in k.items():
+        print(za, zu)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=finlist)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
